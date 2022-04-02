3e34b2c2b67e308d42ba7f7bf9b134898958eb05
anangyoga <aharwinto@gmail.com>
2022-04-03 03:31:45 +0700

    manipulasi dom dengan useRef

--- public/app.js
+++ public/app.js
@@ -1,10 +1,19 @@
-const root = document.querySelector("#root");
+const root = document.querySelector("#root"); // beda useRef dan useState
+// useState ketika initial valuenya diset, terus diganti maka akan re-render
+// useRef tidak melakukan re-render meski initial valuenya udah diganti
 
 function App() {
-  const [login, setLogin] = React.useState(false);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Login Dulu!"), /*#__PURE__*/React.createElement("p", null, " ", login && "Saya sudah login"), /*#__PURE__*/React.createElement("button", {
-    onClick: () => setLogin(true)
-  }, "Login"));
+  // jika diconsole.log, isinya object dengan isi CURRENT doang
+  const judulRef = React.useRef(null);
+  React.useEffect(() => {
+    setTimeout(() => {
+      // CURRENT digunakan manipulasi DOM
+      judulRef.current.textContent = "Aplikasi";
+    }, 2000); // empty array agar eksekusi 1 kali doang pada saat komponen dirender
+  }, []);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    ref: judulRef
+  }, "Application"));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const root = document.querySelector("#root");
 
+// beda useRef dan useState
+// useState ketika initial valuenya diset, terus diganti maka akan re-render
+// useRef tidak melakukan re-render meski initial valuenya udah diganti
+
 function App() {
-  const [login, setLogin] = React.useState(false);
+  // jika diconsole.log, isinya object dengan isi CURRENT doang
+  const judulRef = React.useRef(null);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      // CURRENT digunakan manipulasi DOM
+      judulRef.current.textContent = "Aplikasi";
+    }, 2000);
+
+    // empty array agar eksekusi 1 kali doang pada saat komponen dirender
+  }, []);
 
   return (
     <>
-      <h1>Login Dulu!</h1>
-      <p> {login && "Saya sudah login"}</p>
-      <button onClick={() => setLogin(true)}>Login</button>
+      <h1 ref={judulRef}>Application</h1>
     </>
   );
 }
