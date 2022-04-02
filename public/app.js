cfaa6ba51a349ffcae0dbda4a7a8dc9eb63f6af8
anangyoga <aharwinto@gmail.com>
2022-04-03 04:07:15 +0700

    render list

--- public/app.js
+++ public/app.js
@@ -1,19 +1,8 @@
-const root = document.querySelector("#root"); // beda useRef dan useState
-// useState ketika initial valuenya diset, terus diganti maka akan re-render
-// useRef tidak melakukan re-render meski initial valuenya udah diganti
+const root = document.querySelector("#root");
 
 function App() {
-  // jika diconsole.log, isinya object dengan isi CURRENT doang
-  const judulRef = React.useRef(null);
-  React.useEffect(() => {
-    setTimeout(() => {
-      // CURRENT digunakan manipulasi DOM
-      judulRef.current.textContent = "Aplikasi";
-    }, 2000); // empty array agar eksekusi 1 kali doang pada saat komponen dirender
-  }, []);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    ref: judulRef
-  }, "Application"));
+  const fruits = ["Apple", "Orange", "Pear", "Watermelon"];
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("ul", null, fruits.map(fruit => /*#__PURE__*/React.createElement("li", null, fruit))));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,25 +1,14 @@
 const root = document.querySelector("#root");
 
-// beda useRef dan useState
-// useState ketika initial valuenya diset, terus diganti maka akan re-render
-// useRef tidak melakukan re-render meski initial valuenya udah diganti
-
 function App() {
-  // jika diconsole.log, isinya object dengan isi CURRENT doang
-  const judulRef = React.useRef(null);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      // CURRENT digunakan manipulasi DOM
-      judulRef.current.textContent = "Aplikasi";
-    }, 2000);
-
-    // empty array agar eksekusi 1 kali doang pada saat komponen dirender
-  }, []);
-
+  const fruits = ["Apple", "Orange", "Pear", "Watermelon"];
   return (
     <>
-      <h1 ref={judulRef}>Application</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li>{fruit}</li>
+        ))}
+      </ul>
     </>
   );
 }
