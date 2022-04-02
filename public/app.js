a8ca4ae0abb53daff3f26d8c893cff8a2babb0f1
anangyoga <aharwinto@gmail.com>
2022-04-03 05:38:15 +0700

    form w/ useRef | uncondtrolled component

--- public/app.js
+++ public/app.js
@@ -1,8 +1,26 @@
 const root = document.querySelector("#root");
 
 function App() {
-  const fruits = ["Apple", "Orange", "Pear", "Watermelon"];
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("ul", null, fruits.map(fruit => /*#__PURE__*/React.createElement("li", null, fruit))));
+  // sebenarnya bisa pake getElementById, hanya karena sudah pernah belajar useRef untuk manipulasi DOM JS, jadi kita menggunakan useRef
+  const namaRef = React.useRef(null);
+
+  const submit = event => {
+    event.preventDefault(); // sama aja kaya vanillaJs = namaRef.target.value
+
+    const nama = namaRef.current.value;
+    console.log(`Nama ${nama}`);
+  };
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("form", {
+    onSubmit: submit
+  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Nama Anda: "), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "nama",
+    ref: namaRef
+  })), /*#__PURE__*/React.createElement("input", {
+    type: "submit",
+    value: "Kirim"
+  })));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const root = document.querySelector("#root");
 
 function App() {
-  const fruits = ["Apple", "Orange", "Pear", "Watermelon"];
+  // sebenarnya bisa pake getElementById, hanya karena sudah pernah belajar useRef untuk manipulasi DOM JS, jadi kita menggunakan useRef
+  const namaRef = React.useRef(null);
+
+  const submit = (event) => {
+    event.preventDefault();
+
+    // sama aja kaya vanillaJs = namaRef.target.value
+    const nama = namaRef.current.value;
+    console.log(`Nama ${nama}`);
+  };
   return (
     <>
-      <ul>
-        {fruits.map((fruit) => (
-          <li>{fruit}</li>
-        ))}
-      </ul>
+      <form onSubmit={submit}>
+        <div>
+          <label>Nama Anda: </label>
+          {/* jangan lupa deklarasikan ref */}
+          <input type="text" name="nama" ref={namaRef} />
+        </div>
+        <input type="submit" value="Kirim" />
+      </form>
     </>
   );
 }
